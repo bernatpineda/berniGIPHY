b329c8e7aea2937d06a191e04a95b18bfeb73127
bernat <bernatpinedatorrens@gmail.com>
2023-02-06 13:28:14 +0100

    auth0-initial

--- berni-giphy/src/App.js
+++ berni-giphy/src/App.js
@@ -1,22 +1,25 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { Profile } from './Profile';
 import logo from './logo.svg';
 import './App.css';
+import { LogoutButton } from './Logout';
+import { LoginButton } from './Login';
 
 function App() {
+  const {isAuthenticated} = useAuth0();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isAuthenticated ? (
+        <> 
+        
+        <LogoutButton />
+        <Profile />
+        </>
+   ) : (
+        <LoginButton />
+   )}
       </header>
     </div>
   );
--- berni-giphy/src/index.js
+++ berni-giphy/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider 
+    domain='bernatpineda.eu.auth0.com' 
+    clientId='tGezvSiREMPed2AKQwm1VNj3C2uUVKRW' 
+    redirectUri={window.location.origin}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
